6245603c5f89b98407164ce8cd47bd8aeea32261
Emerson-Gelobter <90862013+Emerson-Gelobter@users.noreply.github.com>
2022-11-23 12:39:36 -0500

    Add files via upload

--- 20_restapi$/app.py
+++ 20_restapi$/app.py
@@ -1,17 +1,35 @@
-from flask import Flask
-from flask import render_template
-from flask import request
+'''
+Nada Hameed and Emerson Gelobter
+SoftDev pd07
+K20 - A RESTful Journey Skyward
+2022-11-21
+time spent: 0.7 hrs
+'''
+
+#imports
+from flask import Flask, render_template
+from urllib import request
+import json
 app = Flask(__name__)
-import requests
 
+#key
+with open("key_nasa.txt", "r") as file:
+    key = file.read()
 
-@app.route("/", methods =['GET', 'POST'])
-def show():
-    print("hello")
-    key = open("key_nasa.txt").read
-    print("hell")
-    url = "https://api.nasa.gov/planetary/apod?api_key=" + key
-    print("hel")
-    x = requests.get(url)
-    print("he")
-    return render_template('main.html', data = x)
+#img
+@app.route("/")
+def img():
+    #f string, it doesn't work if it's not an f string
+    url = f"https://api.nasa.gov/planetary/apod?api_key={key}"
+    #opening the image
+    openimg = request.urlopen(url)
+
+    #we need a dictionary? how come we need to turn a json object to a python dict object?
+    dictionary = json.loads(openimg.read()) #can't do .text bc: the JSON object must be str, bytes or bytearray, not HTTPResponse
+
+    #HOW WE GET THE IMAGE TO DISPLAY:
+    return render_template('main.html',a=dictionary['url'])
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
