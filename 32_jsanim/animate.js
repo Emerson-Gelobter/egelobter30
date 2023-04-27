684ded0ed380beafeed608a33d3aa7ea4a19295a
Emerson-Gelobter <90862013+Emerson-Gelobter@users.noreply.github.com>
2023-04-27 12:45:28 -0400

    Add files via upload

--- 32_jsanim/animate.js
+++ 32_jsanim/animate.js
@@ -1,3 +1,12 @@
+// Team ED Exquisite Dingos: Emerson, Daniel
+// SoftDev pd7
+// K32 JS Animation
+// 2023-04-26
+// --------------------------------------------------
+//send diagnostic output to console
+//(Ctrl-Shift-K in Firefox to reveal console)
+
+
 var c = document.getElementById("playground");
 var dotButton = document.getElementById("circle");
 var dvdButton = document.getElementById("dvd");
@@ -5,88 +14,92 @@ var stopButton = document.getElementById("stop");
 
 var ctx = c.getContext("2d");
 
-ctx.fillStyle = "red";
-
 var requestID;
 
-var radius = 0;
+var radius = 100;
+var radius_inc = 1;
 var growing = true;
-var animating = false;
 
-var drawDot = (e) => {
-    if (e.type == "click" && animating) {
-        return
-    }
-    animating = true;
-    clear();
-
-    ctx.beginPath();
-    ctx.arc(c.width / 2, c.height / 2, radius, 0, Math.PI * 2);
-    ctx.fillStyle = "red";
-    ctx.fill();
-
-    if (growing == true){
-        radius += 1;
-    }
-    else {
-        radius -= 1;
-    }
-    if (radius > c. width / 2){
-        growing = false;
-    }
-    if (radius == 0){
-        growing = true;
-    }
-    requestID = window.requestAnimationFrame(drawDot);
+var drawCircle = () => {
+  ctx.beginPath();
+  ctx.fillStyle = "green";
+  ctx.arc(c.width/2,c.height/2,radius,0,2 * Math.PI);
+  ctx.stroke()
+  ctx.fill()
 }
 
-var stopIt = function() {
-    window.cancelAnimationFrame(requestID);
-    animating = false;
-}
-
-var clear = funtion(e){
-  e.preventDefault();
+var clear = function(e){
+  //e.preventDefault();
   ctx.clearRect(0,0,500,500);
 };
 
-var dvdLogoSetup = function(){
+var drawDot = (e) => {
   window.cancelAnimationFrame(requestID);
-  var rectWidth = 100;
-  var rectHeight = 50;
-  var rectX = Math.floor(Math.random(c.width));
-  var rectY = Math.floor(Math.random(c.height));
-
-  var xVel = 2;
-  var yVel = 2;
-  var logo = new Image();
-  logo.src = "logo_dvd.jpg"
-  var dvdLogo = function(){
-    ctx.clearRect(0, 0, c.width, c.height);
-    //ctx.fillRect(rectX, rectY, rectWidth, rectHeight);
-    ctx.drawImage(logo, rectX, rectY, rectWidth, rectHeight);
-    if(rectX >= c.width || rectX<=0){
-      xVel *= -1 ;
-
-    }
-    if(rectY >=c.height || rectY<=0){
-      yVel *= -1 ;
-    }
-    rectX = rectX + xVel;
-    rectY = rectY + yVel;
-    requestID = window.requestAnimationFrame(dvdLogoSetup());
-
-  };
-  dvdLogo();
-
+  requestID = window.requestAnimationFrame(drawDot);
+  clear();
+  drawCircle();
+  if (growing) {
+      if (radius > c.width / 2) { //if too big
+          growing = false;
+      }
+      radius += radius_inc;
+  }
+  else {
+      radius -= radius_inc;
+      if (radius == 0) {  //other side when growing is set to false
+          growing = true
+      }
+  }
 };
 
 var stopIt = function() {
     window.cancelAnimationFrame(requestID);
+}
 
+var dvdLogoSetup = function(){
+  window.cancelAnimationFrame(requestID);
+
+  var rectWidth = 100;
+  var rectHeight = 50;
+
+  var rectX = Math.floor(Math.random() * (c.width - rectWidth));
+  var rectY = Math.floor(Math.random() * (c.height - rectHeight));
+  //console.log(rectX);
+  //console.log(rectY);
+
+  var xVel = 2;
+  var yVel = 2;
+
+  var logo = new Image();
+  logo.src = "logo_dvd.jpg";
+
+  var dvdLogo = function(){
+    ctx.clearRect(0, 0, c.width, c.height);
+    ctx.fillRect(rectX, rectY, rectWidth, rectHeight);
+    ctx.drawImage(logo, rectX, rectY, rectWidth, rectHeight);
+
+    if(rectX >= (c.width - rectWidth) || rectX <= 0){
+      xVel *= -1 ;
+    }
+
+    if(rectY >=(c.height - rectHeight) || rectY <= 0){
+      yVel *= -1 ;
+    }
+
+    rectX += xVel;
+    rectY += yVel;
+
+    requestID = window.requestAnimationFrame(dvdLogo);
+  };
+  dvdLogo();
+};
+
+var stopIt = function() {
+    console.log(requestID);
+    window.cancelAnimationFrame(requestID);
 };
 
 
 dotButton.addEventListener("click", drawDot);
-stopButton.addEventListener("click", stopIt);
 dvdButton.addEventListener("click", dvdLogoSetup);
+stopButton.addEventListener("click", stopIt);
